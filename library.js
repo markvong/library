fed866606e7d4c7752caeb744f0b1e6b2f36ebd5
AMan <man.alex1995@gmail.com>
2020-04-24 00:28:08 -0700

    updated controllers and logic

--- library.js
+++ library.js
@@ -3,24 +3,76 @@ let myLibrary = [];
 function Book(title, author, numPages, read, index) {
     this.title = title;
     this.author = author;
-    this.numPages = numPages;
-    this.read = this.read ? 'Has already read' : 'Has not yet read';
+    this.pages = numPages;
+    this.read = read;
     this.index = index;
 
     this.info = function() {
+        let hasRead = this.read ? 'Has already read' : 'Has not yet read';
         return this.title + ' by ' + this.author + 
-        ', ' + this.numPages + ' pages, ' + this.read;
+        ', ' + this.pages + ' pages, ' + hasRead;
+    }
+
+    this.hasRead = function() {
+        return this.read ? 'Has already read' : 'Has not yet read'
+    }
+
+    this.toggleRead = function() {
+        if (this.read) this.read = false;
+        else this.read = true;
+        console.log(this.read);
     }
 }
 
-const addBookButton = document.querySelector('.add-button');
-addBookButton.addEventListener('click', () => {
-
-});
 
 initializeFillerData();
 render();
 
+const addBookButton = document.querySelector('.add-button');
+addBookButton.addEventListener('click', () => {
+    let title   = document.forms['book-form']['title'].value;
+    let author  = document.forms['book-form']['author'].value;
+    let pages   = document.forms['book-form']['pages'].value;
+    let read    = document.forms['book-form']['read'].value;
+    console.log('read: ' + read);
+    if (validateForm(title, author, pages, read)) {
+        addBookToLibrary(title, author, pages, read);
+        render();
+        clearForm();    
+    }
+});
+
+function validateForm(title, author, pages, read) {
+    console.log(pages);
+    if (title === '') {
+        alert('Please enter a title');
+        return false
+    } else if (author === '') {
+        alert('Please enter an author');
+        return false;
+    } else if (pages <= 0 || pages === '') {
+        alert('Please enter a valid page count');
+        return false;
+    } else if (read === '') {
+        alert('Please enter if book has been read');
+        return false;
+    }
+    else return true;
+}
+
+function clearForm() {
+    document.forms['book-form']['title'].value = '';
+    document.forms['book-form']['author'].value = '';
+    document.forms['book-form']['pages'].value = '';
+    document.forms['book-form']['read'].value = '';
+}
+
+function deleteBook() {
+    console.log(this.id);
+    myLibrary.splice(this.id, 1);
+    console.log(myLibrary);
+    render();
+}
 
 function addBookToLibrary(title, author, pages, read) {
     let index = myLibrary.length;
@@ -30,6 +82,7 @@ function addBookToLibrary(title, author, pages, read) {
 
 function render() {
     let library = document.querySelector('.library');
+    library.innerHTML = ''; // uncomment to remove test css
     for (let i=0; i<myLibrary.length; i++) {
         console.log(myLibrary[i].info());
         let bookTab = document.createElement('div');
@@ -39,6 +92,7 @@ function render() {
         bookTitle.textContent = myLibrary[i].title;
         bookAuthor.textContent = myLibrary[i].author;
         bookPages.textContent = myLibrary[i].pages;
+        console.log(myLibrary[i].pages);
 
         bookTab.appendChild(bookTitle);
         bookTab.appendChild(bookAuthor);
@@ -46,12 +100,20 @@ function render() {
 
         let optionTab = document.createElement('div');
         let bookRead = document.createElement('label');
-        bookRead.textContent = myLibrary[i].read;
+        bookRead.textContent = myLibrary[i].hasRead();
         
         let deleteButton = document.createElement('button');
         let toggleButton = document.createElement('button');
+
         deleteButton.textContent = 'Delete';
+        deleteButton.id = ''+i;
         toggleButton.textContent = 'Toggle';
+
+        deleteButton.addEventListener('click', deleteBook);
+        toggleButton.addEventListener('click', () => {
+            myLibrary[i].toggleRead();
+            bookRead.textContent = myLibrary[i].hasRead();
+        });
 
         optionTab.appendChild(bookRead);
         optionTab.appendChild(deleteButton);
